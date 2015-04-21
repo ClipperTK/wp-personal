627fb2b44bba146fb64886953e0653c1ab30ae82
Alex King <alex@alexking.org>
2015-04-21 09:57:36 -0600

    update package info

--- functions/gallery.php
+++ functions/gallery.php
@@ -2,11 +2,14 @@
 /**
  * @package favepersonal
  *
- * This file is part of the FavePersonal Theme for WordPress
- * http://crowdfavorite.com/favepersonal/
+ * This file is part of the Personal Theme for WordPress
+ * http://github.com/alexkingorg/wp-personal
+ * (Forked from http://crowdfavorite.com/favepersonal/)
  *
  * Copyright (c) 2008-2013 Crowd Favorite, Ltd. All rights reserved.
  * http://crowdfavorite.com
+ *
+ * Copyright (c) 2015 Alex King.
  *
  * **********************************************************************
  * This program is distributed in the hope that it will be useful, but

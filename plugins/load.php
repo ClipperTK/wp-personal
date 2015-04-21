441364d0908f93806b6799f83fff8595b7126ffc
Alex King <alex@alexking.org>
2015-04-21 12:29:03 -0600

    forked color submodule

--- plugins/load.php
+++ plugins/load.php
@@ -1,5 +1,5 @@
 <?php
 
-include_once('cf-colors/cf-colors.php');
+include_once('wp-colors/cf-colors.php');
 include_once('cf-post-formats/cf-post-formats.php');
 include_once('cf-archive-title/cf-archive-title.php');
